93bba5996a61dad7429dbc49fb827dd407f0a505
eroeb <97666589+eroeb@users.noreply.github.com>
2023-10-05 10:40:00 +0200

    muli

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <div className="App">
-            <MyNavbar />
+            
         </div>
     );
-}
-
-function MyNavbar() {
-    return <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-        <Container>
-            <NavLink to="/" onlyActiveOnIndex exact activeClassName="active">
-                <Navbar.Brand href="" >ejuet</Navbar.Brand>
-            </NavLink>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link>
-                        <NavLink to="/">Home</NavLink>
-                    </Nav.Link>
-                </Nav>
-                <Nav>
-                    <NavLink to="/datenschutzerklaerung">Datenschutzerklärung</NavLink>
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,20 +5,62 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider, createHashRouter } from 'react-router-dom';
 
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import LocalizedStrings from 'react-localization';
+
+const strings = new LocalizedStrings({
+    en:{
+        privacyPolicy:"Privacy Policy"
+    },
+    de:{
+        privacyPolicy:"Datenschutzerklärung"
+    }
+})
 
 const router = createHashRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/datenschutzerklaerung",
-    element: <p>Aaa</p>,
-  },
+    {
+        path: "/",
+        element: <WithNavbar>
+            <App />
+        </WithNavbar>,
+    },
+    {
+        path: "/privacy",
+        element: <p>Aaa</p>,
+    },
 ]);
 
+function WithNavbar({ children }) {
+    return <>
+        <MyNavbar />
+        {children}
+    </>
+}
+
+function MyNavbar() {
+    return <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+        <Container>
+            <NavLink to="/" onlyActiveOnIndex exact activeClassName="active">
+                <Navbar.Brand href="" >ejuet</Navbar.Brand>
+            </NavLink>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link>
+                        <NavLink to="/">Home</NavLink>
+                    </Nav.Link>
+                </Nav>
+                <Nav>
+                    <NavLink to="/privacy">{strings.privacyPolicy}</NavLink>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+    <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
