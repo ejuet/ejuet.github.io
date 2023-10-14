856e4cf0a02c60c9df70f4cba5467a91eb90da12
eroeb <97666589+eroeb@users.noreply.github.com>
2023-10-14 16:32:25 +0200

    clean

--- src/useScrollbarActive.js
+++ src/useScrollbarActive.js
@@ -5,7 +5,6 @@ export function useScrollbarActive() {
 
     useEffect(() => {
         const resizeObserver = new ResizeObserver(entries => {
-            console.log('Body height changed:', entries[0].target.clientHeight);
             setSc(document.body.clientHeight > window.innerHeight);
         });
         resizeObserver.observe(document.body);
